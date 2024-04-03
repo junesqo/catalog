eebe5b1527993a1bed84156f0fce24a023c92009
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2024-04-03 17:16:25 +0600

    inital4

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
 
